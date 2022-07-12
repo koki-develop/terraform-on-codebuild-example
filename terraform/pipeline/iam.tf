ff9fbf10a92707fe39c632acccccd2301fde286f
koki-develop <kou.pg.0131@gmail.com>
2022-07-12 20:05:25 +0900

    codebuild 用の iam role を作成

---
+++ terraform/pipeline/iam.tf
@@ -0,0 +1,34 @@
+resource "aws_iam_role" "codebuild" {
+  name               = "${local.prefix}-codebuild-role"
+  assume_role_policy = data.aws_iam_policy_document.codebuild_assume_role_policy.json
+}
+
+data "aws_iam_policy_document" "codebuild_assume_role_policy" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["codebuild.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_policy" "codebuild" {
+  name   = "${local.prefix}-codebuild-policy"
+  policy = data.aws_iam_policy_document.codebuild_policy.json
+}
+
+data "aws_iam_policy_document" "codebuild_policy" {
+  statement {
+    effect = "Allow"
+    # TODO: 直す
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "codebuild" {
+  role       = aws_iam_role.codebuild.name
+  policy_arn = aws_iam_policy.codebuild.arn
+}
