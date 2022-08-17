b147598d2b824c2846b6b2fdaddc9b63a0d6d051
koki-develop <kou.pg.0131@gmail.com>
2022-08-17 12:01:05 +0900

    3 つめのバケットを作成

--- terraform/example/s3.tf
+++ terraform/example/s3.tf
@@ -25,3 +25,17 @@ resource "aws_s3_bucket_public_access_block" "default2" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket" "default3" {
+  bucket_prefix = "${local.prefix}-3-"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_public_access_block" "default3" {
+  bucket = aws_s3_bucket.default3.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
